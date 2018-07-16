75e06a3c79651c3bfad9673ad618c04e976115fe
Tan Ahmed <tan@popcorndesign.co.uk>
2018-07-16 22:21:21 +0100

    0.2

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+  function playSound(e) {
+    const audio =
+      document.querySelector(`audio[data-key="${e.keyCode}"]`) ||
+      document.querySelector(`audio[data-key="${event.target.dataset.key}"]`);
+    const key =
+      document.querySelector(`div[data-key="${e.keyCode}"]`) ||
+      document.querySelector(`div[data-key="${event.target.dataset.key}"]`);
+    if (!audio) return;
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
+  }
+
+  const keys = Array.from(document.querySelectorAll(".key"));
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+  //playsound() is a listener which is registered using addEventListener
+  //window is global object in a browser
+
+  window.addEventListener("keydown", playSound);
+  window.addEventListener("click", playSound);
+
+  // window.addEventListener("click", function (event) {
+  //     console.log(event.target.dataset.key);
+  // });
+});
